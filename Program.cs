50a048663a52f2f787da832208cd06ff39a19a43
Adelmo Silva <adelmotocomsono@gmail.com>
2021-11-21 04:49:11 -0300

    Única versão

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿{
+    Console.Write("Informe o primeiro valor: ");
+    double n1 = double.Parse(Console.ReadLine());
+    Console.Write("Informe uma operação: ");
+    string op = Console.ReadLine();
+    
+    if (op != "+" )
+    {
+        if (op != "-")
+        {
+            if (op != "*")
+            {
+                if (op != "/")
+                {
+                    Console.WriteLine("Error!");
+                    Console.WriteLine("Escolha uma das seguintes operações: + , - , * , / ");
+                    op = Console.ReadLine();
+                }
+            }
+        }
+    }
+
+    Console.Write("Informe o segundo valor: ");
+    double n2 = double.Parse(Console.ReadLine());
+    double result = 0;
+
+    switch(op)
+    {
+        case "+": 
+                    result = n1 + n2;
+                    Console.Write("O resultado é " + result);                    
+            break;
+
+        case "-":
+                    result = n1 - n2;
+                    Console.Write("O resultado é " + result);
+            break;
+
+        case "*":
+                    result = n1 * n2;
+                    Console.Write("O resultado é " + result);
+            break;
+        
+        case "/":
+                    result = n1 / n2;
+                    Console.Write("O resultado é " + result);
+            break;
+
+        default:
+                    Console.Write("Error!");
+            break;   
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Desafio: Construa um algoritmo que leia dois números e uma operação ( + , - , * , / ), em seguida o algoritmo deve fazer o cálculo utilizando a operação informada.
